f22374069faf74e45daa6231746215f19e9c0f27
Сергей Арефьев <Simpson6515@yandex.ru>
2024-07-08 14:54:14 +0500

    25.1 access rights

--- lms/views.py
+++ lms/views.py
@@ -7,8 +7,10 @@ from rest_framework.generics import (
     UpdateAPIView,
     DestroyAPIView,
 )
+from rest_framework.permissions import IsAuthenticated
 
 from lms.models import Course, Lesson
+from lms.permissions import IsModerator, IsOwner
 from lms.serializers import CourseSerializer, LessonSerializer
 
 
@@ -16,25 +18,61 @@ class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
+    def get_queryset(self):
+        if IsModerator().has_permission(self.request, self):
+            return Course.objects.all()
+        else:
+            return Course.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        lesson = serializer.save()
+        lesson.owner = self.request.user
+        lesson.save()
+
+    def get_permission(self):
+        if self.action in ['update', 'partial_update', 'list', 'retrieve']:
+            self.permission_classes = [IsAuthenticated, IsModerator | IsOwner]
+        if self.action in 'create':
+            self.permission_classes = [IsAuthenticated, ~IsModerator]
+        if self.action in 'destroy':
+            self.permission_classes = [IsAuthenticated, ~IsModerator | IsOwner]
+        return super().get_permissions()
+
 
 class LessonCreateAPIView(CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, ~IsModerator | IsOwner]
+    
+    def perform_create(self, serializer):
+        lesson = serializer.save()
+        lesson.owner = self.request.user
+        lesson.save()
 
 
 class LessonListAPIView(ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if IsModerator().has_permission(self.request, self):
+            return Lesson.objects.all()
+        else:
+            return Lesson.objects.filter(owner=self.request.user)
 
 
 class LessonRetrieveAPIView(RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
 
 
 class LessonUpdateAPIView(UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
 
 
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated, ~IsModerator | IsOwner]
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+
 from users.views import UsersViewSet, PaymentListAPIView
 
 from users.apps import UsersConfig
@@ -10,5 +13,7 @@ router = DefaultRouter()
 router.register(r"user", UsersViewSet, basename="user")
 
 urlpatterns = [
-                path('payments/', PaymentListAPIView.as_view(), name='payments')
+                path('payments/', PaymentListAPIView.as_view(), name='payments'),
+                path('token/', TokenObtainPairView.as_view(permission_classes=[AllowAny]), name='token_obtain_pair'),
+                path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
               ] + router.urls
